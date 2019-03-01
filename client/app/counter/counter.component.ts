91d699020d61ae8d5fd8094b1be16753b709af9a
george <george.dorusomcutean@realitymine.com>
2019-03-01 10:42:51 +0000

    Updated the Bill design.

--- client/app/counter/counter.component.ts
+++ client/app/counter/counter.component.ts
@@ -14,6 +14,8 @@ export class CounterComponent implements OnInit {
     /** Table Object */
     // @Input() table: any;
 
+    // @Input() waiterId: number;
+
     // dummy data for input table
     public table = {
         id: 1,
@@ -21,8 +23,21 @@ export class CounterComponent implements OnInit {
 
     };
 
+    // dummy data for now
+    public waiterId = 1;
+
     // dummy data for order
     public order = [
+        {
+            name: 'Chicken Soup',
+            quantity: 1,
+            price: 7
+        },
+        {
+            name: 'Curry',
+            quantity: 2,
+            price: 11.99
+        },
         {
             name: 'Chicken Soup',
             quantity: 1,
@@ -107,12 +122,13 @@ export class CounterComponent implements OnInit {
     }
 
     /**
-     * Creates the receipts and downloads it
+     * Creates the receipt and downloads it
      */
     printReceipt() {
         const date = new Date();
 
         const text = [];
+
         const lineHeight = 1.2,
             margin = 0.1,
             fontSize = 9,
@@ -124,21 +140,23 @@ export class CounterComponent implements OnInit {
                 format: 'credit-card'
             }).setProperties({ title: 'Receipt' });
 
+        // Put all order items into an array
         this.order.forEach(element => {
-            const itemPrice = element.price * element.quantity;
-            const item = '£' + itemPrice.toFixed(2) + ' x ' + element.quantity + ' - ' + element.name + '\n';
+            const item = '£' + element.price.toFixed(2) + ' x ' + element.quantity + ' - ' + element.name + '\n';
             text.push(item.toString());
         });
 
+        // Set font properties
         doc
             .setFont('helvetica', 'neue')
             .setFontSize(fontSize);
 
-        // doc.text can now add those lines easily; otherwise, it would have run text off the screen!
+        // add items to receipt
         doc.text(text, margin, margin + 4 * oneLineHeight);
 
-        // You can also calculate the height of the text very simply:
+        // Calculate the height of the text very simply:
         const textHeight = text.length * fontSize * lineHeight / ptsPerInch;
+
         doc.setFontStyle('bold')
             .text('Table ' + this.table.table_number + ' Receipt', 1, margin + oneLineHeight, 'center');
 
@@ -152,17 +170,24 @@ export class CounterComponent implements OnInit {
                 .text('Total: £' + this.totalNoDiscount.toFixed(2), 0.8, textHeight + 1, 'right');
         }
 
-        // Show total
+        // Show thank you message
         doc.setFontStyle('bold')
             .text('Thank you for choosing us.', 1, textHeight + 1.5, 'center');
+
         doc.setFontStyle('normal')
-            .text('VAT No: 132323 2323 2', 1, textHeight + 1.8, 'center');
+            .text('Served by waiter id: ' + this.waiterId, 1, textHeight + 1.65, 'center');
+
+        // Show VAT example code
+        doc.setFontStyle('normal')
+            .text('VAT No: 132323 2323 2', 1, textHeight + 1.9, 'center');
+
         // Show date
         doc.setFontStyle('italic')
-            .text(date.toLocaleDateString(), 1, textHeight + 2, 'center');
-        // Receipt Id
+            .text(date.toLocaleDateString(), 1, textHeight + 2.1, 'center');
+
+        // Show receipt Id
         doc.setFontStyle('normal')
-            .text('32322123', 1, textHeight + 2.15, 'center');
+            .text('32322123', 1, textHeight + 2.25, 'center');
 
         doc.save('receipt.pdf');
     }
