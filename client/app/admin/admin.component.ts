06dddf3a9b747939289c8ed1afe2bbeb66110c99
Jessica Wong <jessicawong217@gmail.com>
2019-03-04 17:48:51 +0000

    Side nav bar content alignment

--- client/app/admin/admin.component.ts
+++ client/app/admin/admin.component.ts
@@ -34,14 +34,14 @@ export class AdminComponent implements OnInit {
   openStaffNav() {
     document.getElementById("staffBar").style.width = "20%";
     document.getElementById("staffBar").style.display = "block";
-    document.getElementById("menuMain").style.visibility = "hidden";
+    document.getElementById("staffMain").style.visibility = "hidden";
     document.getElementById("analytics").style.marginLeft = "15%";
   }
 
   closeStaffNav() {
     document.getElementById("staffBar").style.width = "0";
     document.getElementById("staffBar").style.display = "none";
-    document.getElementById("menuMain").style.visibility = "visible";
+    document.getElementById("staffMain").style.visibility = "visible";
     document.getElementById("analytics").style.marginLeft = "0";
 
   }
