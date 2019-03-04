6460f87e3434740092ecfaecd6fd05d555ec0fb7
Jessica Wong <jessicawong217@gmail.com>
2019-03-04 14:10:43 +0000

    Responsive front-end

--- client/app/admin/admin.component.ts
+++ client/app/admin/admin.component.ts
@@ -8,12 +8,42 @@ import { AdminService } from './admin.service';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
   constructor(
     protected adminService: AdminService
   ) { }
 
   ngOnInit() {
+
+  }
+
+  openMenuNav() {
+    document.getElementById("menuBar").style.width = "20%";
+    document.getElementById("menuBar").style.display = "block";
+    document.getElementById("menuMain").style.visibility = "hidden";
+    document.getElementById("summary").style.marginLeft = "15%";
+  }
+
+  closeMenuNav() {
+    document.getElementById("menuBar").style.width = "0";
+    document.getElementById("menuBar").style.display = "none";
+    document.getElementById("menuMain").style.visibility = "visible";
+    document.getElementById("summary").style.marginLeft = "0";
+
+  }
+
+  openStaffNav() {
+    document.getElementById("staffBar").style.width = "20%";
+    document.getElementById("staffBar").style.display = "block";
+    document.getElementById("menuMain").style.visibility = "hidden";
+    document.getElementById("analytics").style.marginLeft = "15%";
+  }
+
+  closeStaffNav() {
+    document.getElementById("staffBar").style.width = "0";
+    document.getElementById("staffBar").style.display = "none";
+    document.getElementById("menuMain").style.visibility = "visible";
+    document.getElementById("analytics").style.marginLeft = "0";
+
   }
 
 }
