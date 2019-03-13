0405bd0a406a17f62ee36010ec2563f871457a45
Ashley Burgess <me@ashleyburg.es>
2019-03-13 11:12:35 +0000

    fix: use delete http call in menu service.

--- client/app/menu/menu.service.ts
+++ client/app/menu/menu.service.ts
@@ -67,7 +67,7 @@ export class MenuService {
     }
 
     delete(id: string) {
-        return this.httpClient.get<MenuItemViewModel>(
+        return this.httpClient.delete<MenuItemViewModel>(
             environment.apiUrl + 'menu/' + id
         );
     }
