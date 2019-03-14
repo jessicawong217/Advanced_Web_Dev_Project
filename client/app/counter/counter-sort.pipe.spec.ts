6b9102bc36f905ad8e916c8c6d7782afcecc107d
Chloe Hodgson <Chloe.Hodgson@autotrader.co.uk>
2019-03-14 20:31:10 +0000

    Removes unused CSS in waiter view and deletes unused spec files

--- client/app/counter/counter-sort.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { CounterSortPipe } from './counter-sort.pipe';
-
-describe('SortPipe', () => {
-
-    it('create an instance', () => {
-        const pipe = new CounterSortPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
