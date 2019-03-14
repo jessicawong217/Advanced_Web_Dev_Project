47ff0d7cc1807ef02ee231335084852663f84845
Jessica Wong <jessicawong217@gmail.com>
2019-03-14 03:47:22 +0000

    Comments

--- client/app/admin/staff-form/staff-form.component.ts
+++ client/app/admin/staff-form/staff-form.component.ts
@@ -2,10 +2,16 @@ import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { MenuItem } from "../../menu/menu.model";
 import { User } from "../../users/user.model";
 
+/**
+ * Staff Component to handle staff sidebar
+ * @param  selector   Staff form
+ * @param  templateUrl Staff sidebar HTML
+ * @param  styleUrls   Staff sidebar CSS
+ */
 @Component({
-  selector: 'app-staff-form',
-  templateUrl: './staff-form.component.html',
-  styleUrls: ['./staff-form.component.css']
+    selector: 'app-staff-form',
+    templateUrl: './staff-form.component.html',
+    styleUrls: ['./staff-form.component.css']
 })
 export class StaffFormComponent implements OnInit {
 
@@ -21,25 +27,41 @@ export class StaffFormComponent implements OnInit {
 
     userEditing: number;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    /**
+     * Add new user
+     * @param  user New user to be added
+     */
     addUser(user: User) {
         this.userAdded.emit(user);
     }
 
+    /**
+     * Update an existing user
+     * @param  user User info to be updated
+     */
     updateUser(user: User) {
         this.userUpdated.emit(user);
         this.userEditing = null;
     }
 
+    /**
+     * Remove a user
+     * @param  id User id to be searched and removed against
+     */
     removeUser(id: string) {
         this.userRemoved.emit(id);
     }
 
-    enableEditing(id){
+    /**
+     * Enable editing of users
+     * @param  id User id for editing
+     */
+    enableEditing(id) {
         this.userEditing = id;
     }
 
